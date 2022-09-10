c41069bd18cf2d9f763c35689bbaeefdb6db5118
sismitrii <floguerin156@gmail.com>
2022-09-10 19:52:27 +0200

    ✨ [timer clock] : timer for clock is done

--- app.js
+++ app.js
@@ -136,6 +136,7 @@ function timerInit(){
     })
 }
 
+
 let count = 0; // nbr seconds
 let playingTimer;
 
@@ -146,11 +147,29 @@ function playPauseFct(){
         playingTimer = setInterval(()=>{
             count += 1;
             updateDigitalClock();
+            updateClock();
         },1000)
     } else {
         clearInterval(playingTimer);
         playPause.innerText = "play";
     }
+}
+
+function updateClock(){
+    const seconde = count%60;
+    const minute = Math.floor(count/60)%60
+    const hour = Math.floor((count/3600)%24)
+
+
+    if (seconde === 0){
+        document.querySelectorAll('.arrow').forEach((time)=>{
+            time.style.transition = "unset";
+            setTimeout(()=>{time.style.transition = "all 0.05s cubic-bezier(0, 2.09, 0.58, 1"}, 500);
+        })
+    }
+    document.querySelector('.seconde').style.transform = `translate(-100%, -50%) rotate(${(seconde * 6) + 90 }deg)`;
+    document.querySelector('.minute').style.transform = `translate(-100%, -50%) rotate(${(minute * 6)+ (seconde* 0.1) + 90}deg)`;
+    document.querySelector('.hour').style.transform = `translate(-100%, -50%) rotate(${((hour%12)*30) +(minute * 0.5) + 90}deg)`;
 }
 
 function resetFct(){
